5af37c2bad9a08db9f3a1cb98c3942c92f74ad65
MarcosFrMelo <143053350+MarcosFrMelo@users.noreply.github.com>
2025-10-28 15:31:17 -0300

    criação das funções de validação e extração de e-mails do formulário usando expressões regulares em Python.

--- formu.py
+++ formu.py
@@ -1,24 +1,4 @@
 import re
-# Implementar computacionalmente em um linguagem de programação de alto nível, uma 
-# aplicação para validar o preenchimento de um formulário feito por algum usuário. 
-# Para isso você deverá utilizar a bilblioteca de expressões da linguagem de programação, 
-# que em python é "import re". O formulário deverá conter os seguintes campos:
-
-#Nome: no máximo 50 símbolos alfabéticos e espaço [a-zA-Z ].
-#CPF: somente algarismos numéricos [0-9] ou algarismos no formato padrão do 
-#CPF "000.000.000-00", ou seja, com ponto na posições 3 e 7 da string e hífen na posição 11. 
-#E-mail: o nome de usuário deverá ter no mínimo 2 símbolos alfanuméricos, ponto, underline 
-#ou hífen [\w\._-] seguido de '@', que por sua vez concatena com o domínio (utilizar a mesma 
-#regra do nome do usuário). Para terminar, depois do domínio, deverá ter, obrigatoriamente, 
-#um ponto seguido do tipo de registro, que é formado por três símbolos alfabéticos minúsculos 
-#[a-z], e o código do país, que contempla: br, ao, pt, es, de, uk. Lembre-se que domínios 
-#registrados nos EUA não têm código do país e também deve ser reconhecido. 
-#Telefone: dois formatos possíveis, sendo o primeiro constituído por somente 11 números; e 
-#o segundo pelo formato "(00)00000-0000".
-#Além da validação dos campos do formulário, sua aplicação deverá varrer o texto abaixo e 
-#extrair todos os e-mails válidos, ignorando os inválidos. Utilize a expressão regular 
-#apresentada no item 3 para identificar os e-mails válidos. Texto para extração esta no arquivo texto.txt
-
 def validar_nome(nome):
     padrao = r'^[a-zA-ZÀ-ú ]{1,50}$'
     return re.match(padrao, nome) is not None
